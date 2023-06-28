e7dbaa4a7b61ad462f7955100b93316f655e0fc6
codekistone <codekistone@gmail.com>
2023-06-28 20:38:19 +0900

    implement TLS

--- Client.cpp
+++ Client.cpp
@@ -4,24 +4,47 @@
 #include <unistd.h>
 #include <thread>
 
+#define USE_SSL
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+void init_openssl()
+{
+    SSL_load_error_strings();
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+}
+
+void cleanup_openssl()
+{
+    ERR_free_strings();
+    EVP_cleanup();
+}
+
 #define PACKET_SIZE 1024
 
-void proc_recv(int clientSocket) {
-
+void proc_recv(int clientSocket, SSL* ssl) {
     // 서버의 통신 로직을 구현합니다.
     // 예: 메세지 송수신, 데이터 처리 등
     char buffer[PACKET_SIZE];
     while(true) {
         memset(buffer, 0, sizeof(buffer));
-        ssize_t byteRead = recv(clientSocket, buffer, PACKET_SIZE, 0);
-        if (byteRead == -1) {
+
+        size_t bytesRead;
+        #ifdef USE_SSL
+            bytesRead = SSL_read(ssl, (char *)buffer, PACKET_SIZE);
+        #else
+            bytesRead = recv(clientSocket, buffer, PACKET_SIZE, 0);
+        #endif
+
+        if (bytesRead == -1) {
             std::cerr << "Failed to receive response." << std::endl;
             break;
         }
-        if (byteRead == 0) {
+        if (bytesRead == 0) {
             std::cout << "Disconnected to server." << std::endl;
-            // break;
-            exit(0);
+            break;
         }
 
         std::cout << "Received message from server: " << buffer << std::endl;
@@ -39,6 +62,12 @@ int main() {
     std::cout << "input serverPort: ";
     std::cin >> serverPort;
     std::cin.ignore();
+
+    #ifdef USE_SSL
+        // OpenSSL 초기화
+        init_openssl();
+        SSL_CTX* sslContext = SSL_CTX_new(TLS_client_method());
+    #endif
 
     // 소켓 생성
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -64,10 +93,24 @@ int main() {
         return 1;
     }
 
+
+    SSL* ssl;
+    #ifdef USE_SSL
+        // OpenSSL 소켓 초기화
+        ssl = SSL_new(sslContext);
+        SSL_set_fd(ssl, clientSocket);
+
+        // SSL 핸드쉐이크
+        if (SSL_connect(ssl) != 1) {
+            std::cerr << "SSL handshake failed." << std::endl;
+            return 1;
+        }
+    #endif
+
     // 연결이 성공적으로 수행됨
     std::cout << "Connected to server!" << std::endl;
 
-    std::thread recvThread(proc_recv, clientSocket);
+    std::thread recvThread(proc_recv, clientSocket, ssl);
 
     // 서버의 통신 로직을 구현합니다.
     // 예: 메세지 송수신, 데이터 처리 등
@@ -78,7 +121,14 @@ int main() {
         std::string message;
         std::getline(std::cin, message);
 
-        if (send(clientSocket, message.c_str(), message.length(), 0) == -1)
+        ssize_t bytesSend;
+        #ifdef USE_SSL
+            bytesSend = SSL_write(ssl, message.c_str(), message.length());
+        #else
+            bytesSend = send(clientSocket, message.c_str(), message.length(), 0);
+        #endif
+
+        if (bytesSend == -1)
         {
             std::cerr << "Fail to send message" << std::endl;
             break;
@@ -86,26 +136,24 @@ int main() {
 
         if (message.empty()) {
             std::cout << "Terminate connection" << std::endl;
+            pthread_cancel(recvThread.native_handle());
             break;
         }
-
-        // memset(buffer, 0, sizeof(buffer));
-        // ssize_t byteRead = recv(clientSocket, buffer, PACKET_SIZE, 0);
-        // if (byteRead == -1) {
-        //     std::cerr << "Failed to receive response." << std::endl;
-        //     break;
-        // }
-        // if (byteRead == 0) {
-        //     std::cout << "Disconnected to server." << std::endl;
-        //     break;
-        // }
-
-        // std::cout << "Received message from server: " << buffer << std::endl;
     }
     recvThread.join();
 
-    // 연결 종료
+    #ifdef USE_SSL
+        // 연결 종료
+        SSL_shutdown(ssl);
+    #endif
+
     close(clientSocket);
+
+    #ifdef USE_SSL
+        // OpenSSL 정리
+        SSL_CTX_free(sslContext);
+        cleanup_openssl();
+    #endif
 
     return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -7,45 +7,110 @@
 #include <string>
 #include <map>
 
-#define PACKET_SIZE 1024
-const int MAX_CLIENTS = 10;  // 최대 클라이언트 수
-std::map<std::string, int> clientMap;
+#define USE_SSL
 
-void GetClientName(int clientSocket, char* displayName)
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+void init_openssl()
 {
-    struct sockaddr_in clientAddress;
-    socklen_t clientAddressLength = sizeof(clientAddress);
-    getpeername(clientSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
-
-    char clientIP[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIP, INET_ADDRSTRLEN);
-    int clientPort = ntohs(clientAddress.sin_port);
-    std::string portStr = std::to_string(clientPort);
-
-    strcpy(displayName, "(");
-    strcat(displayName, clientIP);
-    strcat(displayName, ":");
-    strcat(displayName, portStr.c_str());
-    strcat(displayName, ")");
-
-    clientMap.insert({displayName, clientSocket});
+    SSL_load_error_strings();
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
 }
 
-void HandleClient(int clientSocket) {
+void cleanup_openssl()
+{
+    ERR_free_strings();
+    EVP_cleanup();
+}
+
+#define PACKET_SIZE 1024
+const int MAX_CLIENTS = 10;  // 최대 클라이언트 수
+
+#ifdef USE_SSL
+    std::map<std::string, SSL*> clientMap;
+
+    void GetClientName(SSL* ssl, int clientSocket, char* displayName)
+    {
+        struct sockaddr_in clientAddress;
+        socklen_t clientAddressLength = sizeof(clientAddress);
+        getpeername(clientSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+
+        char clientIP[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIP, INET_ADDRSTRLEN);
+        int clientPort = ntohs(clientAddress.sin_port);
+        std::string portStr = std::to_string(clientPort);
+
+        strcpy(displayName, "(");
+        strcat(displayName, clientIP);
+        strcat(displayName, ":");
+        strcat(displayName, portStr.c_str());
+        strcat(displayName, ")");
+
+        clientMap.insert({displayName, ssl});
+    }
+#else
+    std::map<std::string, int> clientMap;
+
+    void GetClientName(int clientSocket, char* displayName)
+    {
+        struct sockaddr_in clientAddress;
+        socklen_t clientAddressLength = sizeof(clientAddress);
+        getpeername(clientSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+
+        char clientIP[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIP, INET_ADDRSTRLEN);
+        int clientPort = ntohs(clientAddress.sin_port);
+        std::string portStr = std::to_string(clientPort);
+
+        strcpy(displayName, "(");
+        strcat(displayName, clientIP);
+        strcat(displayName, ":");
+        strcat(displayName, portStr.c_str());
+        strcat(displayName, ")");
+
+        clientMap.insert({displayName, clientSocket});
+    }
+#endif
+
+void HandleClient(int clientSocket, SSL_CTX* sslContext) {
     // 클라이언트와의 통신 처리
     // 예: 메시지 송수신, 데이터 처리 등
+
+    #ifdef USE_SSL
+        // OpenSSL 소켓 초기화
+        SSL* ssl = SSL_new(sslContext);
+        SSL_set_fd(ssl, clientSocket);
+
+        // SSL 핸드쉐이크
+        if (SSL_accept(ssl) != 1) {
+            std::cerr << "SSL handshake failed." << std::endl;
+            return;
+        }
+    #endif
    
     char *displayName = new char[30];
-    GetClientName(clientSocket, displayName);
+
+    #ifdef USE_SSL
+        GetClientName(ssl, clientSocket, displayName);
+    #else
+        GetClientName(clientSocket, displayName);
+    #endif
 
     std::cout << "Connected to client! " << displayName << std::endl;
 
     char buffer[PACKET_SIZE];
     while(true) {
         memset(buffer, 0, sizeof(buffer));
-
         // 클라이언트로부터 메세지 수신
-        ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        ssize_t bytesRead;
+        #ifdef USE_SSL
+            bytesRead = SSL_read(ssl, (char *)buffer, PACKET_SIZE);
+        #else
+            bytesRead = recv(clientSocket, buffer, PACKET_SIZE, 0);
+        #endif
+
         if (bytesRead == -1) {
             std::cerr << "Failed to receive message" << displayName <<std::endl;
             break;
@@ -62,24 +127,51 @@ void HandleClient(int clientSocket) {
         strcpy(sendBuffer, displayName);
         strcat(sendBuffer, ": ");
         strcat(sendBuffer, buffer);
-        for (auto& iter : clientMap) {
-            send(iter.second, sendBuffer, PACKET_SIZE, 0);
-        }
-
-        // const char *response = "this is server.";
-        // if(send(clientSocket, response, strlen(response), 0) == -1) {
-        // if(send(clientSocket, buffer, PACKET_SIZE, 0) == -1) {
-        //     std::cerr << "Failed to send response" << displayName << std::endl;
-        //     break;
-        // }
+        // #ifdef USE_SSL
+        //     for (auto& iter : clientMap) {
+        //         SSL_write(iter.second, sendBuffer, PACKET_SIZE);
+        //     }
+        // #else
+        //     for (auto& iter : clientMap) { 
+        //         send(iter.second, sendBuffer, PACKET_SIZE, 0);
+        //     }
+        // #endif
     }
 
     clientMap.erase(displayName);
+
+    // Client 연결종료
+    #ifdef USE_SSL
+        SSL_shutdown(ssl);
+    #endif
     close(clientSocket);
+
+    #ifdef USE_SSL
+        // OpenSSL 소켓 정리
+        SSL_free(ssl);
+    #endif
 }
 
 int main() {
-    int serverPort = 12345;
+    int serverPort = 443;
+
+    #ifdef USE_SSL
+        // OpenSSL 초기화
+        init_openssl();
+    #endif
+    SSL_CTX *sslContext = SSL_CTX_new(TLS_server_method());
+
+    #ifdef USE_SSL
+        // 인증서 및 개인 키 로드
+        if (SSL_CTX_use_certificate_file(sslContext, "./key/cert.crt", SSL_FILETYPE_PEM) != 1) {
+            std::cerr << "Failed to load certificate file" << std::endl;
+            return 1;
+        }
+        if (SSL_CTX_use_PrivateKey_file(sslContext, "./key/private.key", SSL_FILETYPE_PEM) != 1) {
+            std::cerr << "Failed to load private key file" << std::endl;
+            return 1;
+        }
+    #endif
 
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
@@ -87,19 +179,21 @@ int main() {
         return -1;
     }
 
+    // 서버 주소 설정
     sockaddr_in serverAddress = {};
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(serverPort);
 
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
-    {
+    // 소켓과 서버 주소 바인딩
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
         /* data */
         std::cerr << "Failed to bind." << std::endl;
         close(serverSocket);
         return 1;
     };
 
+    // 소켓을 수신 대기 상태로 전환
     if (listen(serverSocket, MAX_CLIENTS) == -1) {
         std::cerr << "Failed to listen." << std::endl;
     }
@@ -107,6 +201,7 @@ int main() {
     std::cout << "Waiting for client..." << std::endl;
     std::vector<std::thread> clientThread;
     while (true) {
+        // 클라이언트 연결 요청 수락
         sockaddr_in clientAddress{};
         socklen_t clientAddressLength = sizeof(clientAddress);
         int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
@@ -116,12 +211,18 @@ int main() {
         }
 
         // 클라이언트와의 통신을 별도 스레드로 처리
-        std::thread clientThread(HandleClient, clientSocket);
+        std::thread clientThread(HandleClient, clientSocket, sslContext);
         clientThread.detach();
     }
 
     // 서버 소켓 닫기
     close(serverSocket);
+
+    #ifdef USE_SSL
+        // OpenSSL 정리
+        SSL_CTX_free(sslContext);
+        cleanup_openssl();
+    #endif
 
     return 0;
 }
